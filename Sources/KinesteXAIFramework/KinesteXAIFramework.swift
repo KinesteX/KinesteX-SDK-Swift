14891e83c7a7499a0ac80d31ef332b40804d740d
vladimirshetnikov <wowcik.com@gmail.com>
2024-02-21 02:21:45 +0400

    Added callback for HTTPS post messages

--- Sources/KinesteXAIFramework/KinesteXAIFramework.swift
+++ Sources/KinesteXAIFramework/KinesteXAIFramework.swift
@@ -2,37 +2,61 @@
 import SwiftUI
 import WebKit
 
+public enum WebViewMessage {
+    case kinestexLaunched(String)
+    case finishedWorkout(String)
+    case errorOccurred(String)
+    case exerciseCompleted(String)
+    case exitApp(String)
+    case workoutOpened(String)
+    case workoutStarted(String)
+    case planUnlocked(String)
+    case unknown(String) // For handling any unrecognized messages
+}
+
 // Define a single SwiftUI view for both iOS and macOS
 public struct KinesteXAIFramework: View {
     let apiKey: String
     let companyName: String
     let userId: String
+    let planCategory: String
+    let workoutCategory: String
     @Binding var isLoading: Bool
+    var onMessageReceived: (WebViewMessage) -> Void
 
-    public init(apiKey: String, companyName: String, userId: String, isLoading: Binding<Bool>) {
+    public init(apiKey: String, companyName: String, userId: String, planCategory: String, workoutCategory: String, isLoading: Binding<Bool>, onMessageReceived: @escaping (WebViewMessage) -> Void) {
         self.apiKey = apiKey
         self.companyName = companyName
         self.userId = userId
+        self.planCategory = planCategory
+        self.workoutCategory = workoutCategory
         self._isLoading = isLoading
+        self.onMessageReceived = onMessageReceived
     }
     
     public var body: some View {
         #if os(iOS)
-        WebViewWrapperiOS(url: URL(string: "https://kineste-x-w.vercel.app")!, apiKey: apiKey, companyName: companyName, userId: userId, isLoading: $isLoading)
+        WebViewWrapperiOS(url: URL(string: "https://kineste-x-w.vercel.app")!, apiKey: apiKey, companyName: companyName, userId: userId, planCategory: planCategory, workoutCategory: workoutCategory,isLoading: $isLoading, onMessageReceived: onMessageReceived)
         #else
-        WebViewWrappermacOS(url: URL(string: "https://kineste-x-w.vercel.app")!, apiKey: apiKey, companyName: companyName, userId: userId, isLoading: $isLoading)
+        WebViewWrappermacOS(url: URL(string: "https://kineste-x-w.vercel.app")!, apiKey: apiKey, companyName: companyName, userId: userId, planCategory: planCategory, workoutCategory: workoutCategory, isLoading: $isLoading, onMessageReceived: onMessageReceived)
         #endif
     }
 }
 
+
 #if canImport(UIKit)
 import UIKit
+
 struct WebViewWrapperiOS: UIViewRepresentable {
     let url: URL
     let apiKey: String
     let companyName: String
     let userId: String
+    let planCategory: String
+    let workoutCategory: String
+    
     @Binding var isLoading: Bool
+    var onMessageReceived: (WebViewMessage) -> Void
     
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
@@ -45,51 +69,104 @@ struct WebViewWrapperiOS: UIViewRepresentable {
     }
     
     func makeCoordinator() -> Coordinator {
-        Coordinator(parent: self)
+        Coordinator(parent: self, onMessageReceived: onMessageReceived)
     }
     
     class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
         let parent: WebViewWrapperiOS
+        var onMessageReceived: (WebViewMessage) -> Void
         
-        init(parent: WebViewWrapperiOS) {
+        init(parent: WebViewWrapperiOS, onMessageReceived: @escaping (WebViewMessage) -> Void) {
             self.parent = parent
+            self.onMessageReceived = onMessageReceived
         }
         
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
             parent.isLoading = false
+            
             let script = """
             window.postMessage({
                 'key': '\(parent.apiKey)',
                 'company': '\(parent.companyName)',
                 'userId': '\(parent.userId)',
-                'planC': 'Cardio',
+                'planC': '\(parent.planCategory)',
+                'category': '\(parent.workoutCategory)'
+                              
             });
             """
-            // pass the values
             webView.evaluateJavaScript(script) { (result, error) in
                 if let error = error {
-                    print("JavaScript error: \(error)")
+                    DispatchQueue.main.async {
+                        self.onMessageReceived(.errorOccurred("Cannot send data"))
+                    }
                 }
             }
         }
         
         func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-            // Handle received messages here
-        }
+               guard let messageBody = message.body as? String else { return }
+               handle(message: messageBody)
+           }
+           
+           func handle(message: String) {
+               guard let data = message.data(using: .utf8),
+                     let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
+                     let type = json["type"] as? String else {
+                   print("Could not parse JSON message from WebView.")
+                   return
+               }
+               
+               let messageData = json["data"] as? String ?? ""
+               
+               // Using the currentTime function as described, ensure it's defined or adjust as needed
+               
+               let webViewMessage: WebViewMessage
+               switch type {
+               case "kinestex_launched":
+                   webViewMessage = .kinestexLaunched(messageData)
+               case "finished_workout":
+                   webViewMessage = .finishedWorkout("\(messageData)")
+               case "error_occured":
+                   webViewMessage = .errorOccurred("\(messageData)")
+               case "exercise_completed":
+                   webViewMessage = .exerciseCompleted("\(messageData)")
+               case "exitApp":
+                   webViewMessage = .exitApp("User closed workout window")
+               case "workoutOpened":
+                   webViewMessage = .workoutOpened(messageData)
+               case "workoutStarted":
+                   webViewMessage = .workoutStarted(messageData)
+               case "plan_unlocked":
+                   webViewMessage = .planUnlocked(messageData)
+               default:
+                   webViewMessage = .unknown("Unknown message type: \(type) with data: \(messageData)")
+               }
+               
+               DispatchQueue.main.async {
+                   self.onMessageReceived(webViewMessage)
+               }
+           }
+           
+       
     }
 }
 #endif
+
 // iOS specific wrapper
 
 #if canImport(AppKit)
 import AppKit
-// macOS specific wrapper
+
 struct WebViewWrappermacOS: NSViewRepresentable {
     let url: URL
     let apiKey: String
     let companyName: String
     let userId: String
+    let planCategory: String
+    let workoutCategory: String
+    
     @Binding var isLoading: Bool
+    var onMessageReceived: (WebViewMessage) -> Void
     
     func makeNSView(context: Context) -> WKWebView {
         let webView = WKWebView()
@@ -102,27 +179,85 @@ struct WebViewWrappermacOS: NSViewRepresentable {
     }
     
     func makeCoordinator() -> Coordinator {
-        Coordinator(parent: self)
+        Coordinator(parent: self, onMessageReceived: onMessageReceived)
     }
     
     class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
         let parent: WebViewWrappermacOS
+        var onMessageReceived: (WebViewMessage) -> Void
         
-        init(parent: WebViewWrappermacOS) {
+        init(parent: WebViewWrappermacOS, onMessageReceived: @escaping (WebViewMessage) -> Void) {
             self.parent = parent
+            self.onMessageReceived = onMessageReceived
         }
         
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
             parent.isLoading = false
-            let script = "window.postMessage({'apiKey': '\(parent.apiKey)', 'companyName': '\(parent.companyName)', 'userId': '\(parent.userId)'});"
-            webView.evaluateJavaScript(script, completionHandler: nil)
+            let script = """
+            window.postMessage({
+                'key': '\(parent.apiKey)',
+                'company': '\(parent.companyName)',
+                'userId': '\(parent.userId)',
+                'planC': '\(parent.planCategory)',
+                'category': '\(parent.workoutCategory)'
+                              
+            });
+            """
+            webView.evaluateJavaScript(script) { (result, error) in
+                if let error = error {
+                    DispatchQueue.main.async {
+                        self.onMessageReceived(.errorOccurred("Cannot send data"))
+                    }
+                }
+            }
         }
         
         func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-            // Handle received messages here
-        }
+               guard let messageBody = message.body as? String else { return }
+               handle(message: messageBody)
+           }
+           
+           func handle(message: String) {
+               guard let data = message.data(using: .utf8),
+                     let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
+                     let type = json["type"] as? String else {
+                   print("Could not parse JSON message from WebView.")
+                   return
+               }
+               
+               let messageData = json["data"] as? String ?? ""
+               
+               // Using the currentTime function as described, ensure it's defined or adjust as needed
+               
+               let webViewMessage: WebViewMessage
+               switch type {
+               case "kinestex_launched":
+                   webViewMessage = .kinestexLaunched(messageData)
+               case "finished_workout":
+                   webViewMessage = .finishedWorkout("\(messageData)")
+               case "error_occured":
+                   webViewMessage = .errorOccurred("\(messageData)")
+               case "exercise_completed":
+                   webViewMessage = .exerciseCompleted("\(messageData)")
+               case "exitApp":
+                   webViewMessage = .exitApp("User closed workout window")
+               case "workoutOpened":
+                   webViewMessage = .workoutOpened(messageData)
+               case "workoutStarted":
+                   webViewMessage = .workoutStarted(messageData)
+               case "plan_unlocked":
+                   webViewMessage = .planUnlocked(messageData)
+               default:
+                   webViewMessage = .unknown("Unknown message type: \(type) with data: \(messageData)")
+               }
+               
+               DispatchQueue.main.async {
+                   self.onMessageReceived(webViewMessage)
+               }
+           }
+           
     }
 }
-
 #endif
+
 
